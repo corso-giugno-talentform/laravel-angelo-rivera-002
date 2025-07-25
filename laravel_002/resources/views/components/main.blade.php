5b4b82a6b196775f2d1f32fae922c085b59636aa
Arwel219 <aruelangeloriv01@gmail.com>
2025-07-25 17:15:44 +0200

    aggiornamento files

--- laravel_002/resources/views/components/main.blade.php
+++ laravel_002/resources/views/components/main.blade.php
@@ -15,7 +15,6 @@
 </head>
 
 <body class="d-flex flex-column min-vh-100">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
