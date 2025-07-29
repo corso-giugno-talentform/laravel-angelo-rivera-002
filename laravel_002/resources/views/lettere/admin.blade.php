b47dd40da7e67f665c8064cbdca028ef68dae4f0
Arwel219 <aruelangeloriv01@gmail.com>
2025-07-29 12:50:47 +0200

    aggiornamento esercizio con mailtrap

--- laravel_002/resources/views/lettere/admin.blade.php
+++ laravel_002/resources/views/lettere/admin.blade.php
@@ -1,10 +1,31 @@
-Ha condiviso un pensiero questo utente: {{ $data['email'] }}
+<div
+    style="max-width:700px; margin:auto; background:#fff; padding:20px; border-radius:8px; box-shadow:0 2px 5px rgba(0,0,0,0.1);">
+    <div style="background-color:#17a2b8; color:#fff; padding:10px; border-radius:4px 4px 0 0;">
+        <h4 style="margin:0; text-align:center;">Nuovo pensiero condiviso</h4>
+    </div>
 
-Suo Nome -> {{ $data['nome'] }},
+    <div style="padding:15px; color:#333;">
+        <p style="margin-bottom:15px;">
+            <strong>Ha condiviso un pensiero questo utente:</strong> {{ $data['email'] }}
+        </p>
 
-Su -> {{ $data['titolo'] }},
-Con desc -> {{ $data['descrizione'] }}.
+        <ul style="list-style:none; padding:0; margin-bottom:15px;">
+            <li style="background-color:#f8f9fa; margin-bottom:8px; padding:10px; border-radius:4px;">
+                <strong>Suo Nome:</strong> {{ $data['nome'] }}
+            </li>
+            <li style="background-color:#f8f9fa; margin-bottom:8px; padding:10px; border-radius:4px;">
+                <strong>Su:</strong> {{ $data['titolo'] }}
+            </li>
+            <li style="background-color:#f8f9fa; margin-bottom:8px; padding:10px; border-radius:4px;">
+                <strong>Con desc:</strong> {{ $data['descrizione'] }}
+            </li>
+        </ul>
 
-Il suo messaggio:
-
-{{ $data['msg'] }}
+        <div>
+            <h5 style="margin-top:0;">Il suo messaggio:</h5>
+            <div style="border:1px solid #dee2e6; padding:10px; border-radius:4px; background-color:#f8f9fa;">
+                {{ $data['msg'] }}
+            </div>
+        </div>
+    </div>
+</div>
--- laravel_002/resources/views/lettere/email.blade.php
+++ laravel_002/resources/views/lettere/email.blade.php
@@ -1,1 +1,12 @@
-Ciao, grazie per aver condiviso un tuo pensiero sull' articolo {{ $data['titolo'] }}!
+<div
+    style="max-width:600px; margin:auto; background:#fff; padding:20px; border-radius:8px; box-shadow:0 2px 5px rgba(0,0,0,0.1); text-align:center;">
+    <h2 style="color:#007bff; ">Grazie per il tuo pensiero!</h2>
+    <p style="font-size:16px; color:#333;">Ciao, grazie per aver condiviso un tuo pensiero
+        sull'articolo
+        <strong>{{ $data['titolo'] }}</strong>!
+    </p>
+    <br>
+    <br>
+    <br>
+    <x-footer />
+</div>
